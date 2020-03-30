7df7093d90f2a4a0965cdbf2cdbbcb8552dc180c
weynsa <weyns.arno@gmail.com>
2020-03-30 10:27:48 +0200

    update sender port

--- Multicasting/src/multicastSender.java
+++ Multicasting/src/multicastSender.java
@@ -6,7 +6,7 @@ public class multicastSender {
     public static void main(String [] args){
         try {
             InetAddress group = InetAddress.getByName("143.129.39.106");
-            MulticastSocket multicastSocket = new MulticastSocket(3009);
+            MulticastSocket multicastSocket = new MulticastSocket();
             String msg = "Hello group1";
             DatagramPacket packet = new DatagramPacket(msg.getBytes(), msg.length(), group, 3008);
             multicastSocket.send(packet);
